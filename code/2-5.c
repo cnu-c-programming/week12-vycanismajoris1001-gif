7fcf9e150c578c42ffc908e23c987fc3e06a9281
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-05-22 09:58:11 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,17 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "r");
+
+    while ((fp = fgetc(src)) != EOF) {
+        fputc(fp, dst);
+    }
+
+
+    fclose(fp_scr);
+    fclose(fp_dst);
+
 
 }
 
