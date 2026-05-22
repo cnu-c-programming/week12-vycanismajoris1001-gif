2cf5fa746daa298020d5484ee3f1918034bbb9b5
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-05-22 10:53:01 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -5,6 +5,26 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        printf("Error: 파일을 열 수 없습니다: %s\n", argv[1]);
+        return 1;
+    }
+
+    int alphabet_counts[26] = {0};
+    char ch;
+
+    while (!feof(fp)) {
+        if (fscanf(fp, "%c", &ch) == 1) {
+            if (ch >= 'a' && ch <= 'z') {
+                alphabet_counts[ch - 'a']++;
+            }
+        }
+    }
+
+    printf("--- 소문자 알파벳 빈도수 결과 ---\n");
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, alphabet_counts[i]);
+    }
 
 
 
