b2cb656eef8837ce6dc96cc55baa8f4403d73908
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-05-22 10:57:41 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -10,7 +10,38 @@ typedef struct config {
 } Config;
 
 void config_parser(Config* config_ptr) {
+    FILE* fp = fopen("config.txt", "r");
+    if (fp == NULL) {
+        printf("Error: config.txt 파일을 열 수 없습니다.\n");
+        return;
+    }
 
+    char key[64];
+    char dummy[4];
+
+    while (!feof(fp)) {
+        if (fscanf(fp, "%63s %3s", key, dummy) == 2) {
+
+            if (strcmp(key, "InputFileName") == 0) {
+                fscanf(fp, "%63s", config_ptr->InputFileName);
+            } 
+            else if (strcmp(key, "Options") == 0) {
+                fscanf(fp, "%d", &config_ptr->Options);
+            } 
+            else if (strcmp(key, "SectionName") == 0) {
+                fscanf(fp, "%63s", config_ptr->SectionName);
+            } 
+            else if (strcmp(key, "Address") == 0) {
+                fscanf(fp, "%lli", &config_ptr->Address);
+            }
+            else {
+                char skip[128];
+                fscanf(fp, "%127s", skip);
+            }
+        }
+    }
+
+    fclose(fp);
 }
 
 int main(int argc, const char* argv[]) {
