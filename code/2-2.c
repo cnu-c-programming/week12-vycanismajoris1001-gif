7a5922e1c320e9787e2922d00b4f247b0ce5cfb8
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-05-22 10:50:20 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -10,13 +10,34 @@ int main(int argc, const char* argv[]) {
     FILE* fp = fopen("student.txt", "r");
     int count = 0;
     Student students[64];
+    
 
 
 
     int max = 0;
     float avg = 0;
+    int sum = 0;
+
+    while (!feof(fp)) {
+        if (count >= 64) {
+            break;
+        }
 
 
+        if (fscanf(fp, "%63s %d", students[count].name, &students[count].score) == 2) {
+            
+            if (students[count].score > max) {
+                max = students[count].score;
+            }
+            sum += students[count].score;
+            
+            count++; 
+        }
+    }
+
+    if (count > 0) {
+        avg = (float)sum / count; 
+    }
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
